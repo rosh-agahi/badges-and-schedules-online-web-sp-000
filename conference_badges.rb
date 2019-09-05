781ebdd29e82ce40cfbc8ed0c5e6e4b1257320c9
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-05 14:40:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,5 +5,7 @@ def badge_maker(name)
 end
   
 def batch_badge_creator(speakers)
-  speakers.each {|name| "Hello, my name is #{name}."}
+  speakers.each do |name| 
+    "Hello, my name is #{name}."
+  end
 end
